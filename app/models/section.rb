bacca14a64abbe650302fc9ffeadeab5bbda8d8f
Oleg Bavaev <csolg7@gmail.com>
2021-12-30 13:47:41 +1000

    fix: update section form

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,6 @@ class Section < ApplicationRecord
   belongs_to :teacher
   belongs_to :subject
   belongs_to :room
+
+  validates :duration, :start_at, :teacher_id, :subject_id, :room_id, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -10,4 +10,8 @@ class Teacher < ApplicationRecord
   def first_and_last_name
     "#{first_name} #{last_name}"
   end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
